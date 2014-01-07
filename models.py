f78afa1abfd67fd834960c8cf2e53b0f7a3b0b03
Chris <you@example.com>
2014-01-07 21:40:13 +0000

    adding models and development GUI

---
+++ models.py
@@ -0,0 +1,34 @@
+from google.appengine.ext import db
+from google.appengine.api import users
+
+class File(db.Model): 
+  name = db.StringProperty()
+  description = db.StringProperty()
+  content_type = db.StringProperty()
+  encryption_meta = db.TextProperty() # A string of encoded json data used to keep track of the columns encrypted by public key.   
+  # The link to the file contents in the blobstore. 
+  #owner = db.User() #- the person uploading the file.
+  created = db.DateTimeProperty(auto_now_add=True)
+
+class PublicKey(db.Model):
+  name = db.StringProperty()
+  description = db.StringProperty()
+  publickey = db.TextProperty() # public encryption key
+  #owner - the person uploading the file.
+  created = db.DateTimeProperty(auto_now_add=True)
+  
+class ScheduledUpload(db.Model): 
+  name = db.StringProperty()
+  description = db.StringProperty()
+  #start_date
+  #end_date
+  #frequency #String - yearly, monthly, weekly, daily
+  #owner - the person uploading the file.
+  created = db.DateTimeProperty(auto_now_add=True)
+
+class ScheduledUploadFileAssociation(db.Model): 
+  #schedule
+  file = db.ReferenceProperty(File, required=True)
+  created = db.DateTimeProperty(auto_now_add=True)
+
+
---
+++ unittests/test_models.py
@@ -0,0 +1,72 @@
+'''
+Created on Jan 8, 2014
+
+@author: Chris
+'''
+
+from google.appengine.ext import db
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch
+from google.appengine.api import users
+from google.appengine.ext import testbed
+from google.appengine.api.blobstore import blobstore_stub, file_blob_storage
+from google.appengine.api.files import file_service_stub
+from google.appengine.ext import blobstore
+
+from django.utils import simplejson as json
+from Crypto.PublicKey import RSA
+import unittest
+import logging
+import models
+
+class TestbedWithFiles(testbed.Testbed):
+    """See http://stackoverflow.com/questions/8130063/test-function-with-google-app-engine-files-api"""
+
+    def init_blobstore_stub(self):
+        blob_storage = file_blob_storage.FileBlobStorage('/tmp/testbed.blobstore',
+                                                testbed.DEFAULT_APP_ID)
+        blob_stub = blobstore_stub.BlobstoreServiceStub(blob_storage)
+        file_stub = file_service_stub.FileServiceStub(blob_storage)
+        self._register_stub('blobstore', blob_stub)
+        self._register_stub('file', file_stub)
+
+class Test_Models(unittest.TestCase):
+
+    def setUp(self):
+        self.testbed = TestbedWithFiles()
+        #self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        self.testbed.init_mail_stub()
+        self.testbed.init_blobstore_stub()
+        self.testbed.init_taskqueue_stub(root_path="../.") #2.7
+        
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def test_file(self):
+        f = models.File()
+        f.put()
+        self.assertEqual(1,models.File.all().count())
+
+    def test_publickey(self):
+        pk = models.PublicKey()
+        pk.put()
+        self.assertEqual(1,models.PublicKey.all().count())
+
+    def test_ScheduledUpload(self):
+        su = models.ScheduledUpload()
+        su.put()
+        self.assertEqual(1,models.ScheduledUpload.all().count())
+
+    def test_ScheduledUploadFileAssociation(self):
+        f = models.File()
+        f.put()
+        
+        sua = models.ScheduledUploadFileAssociation(file=f)
+        sua.put()
+        self.assertEqual(1,models.ScheduledUploadFileAssociation.all().count())
