10c66ec513edb93d8a9d8be893089a13d654d80a
Damien Lebrun <dinoboff@hotmail.com>
2014-02-27 02:11:55 +0000

    Add a schema resolver
    To share schema definition between apps

--- api/models.py
+++ api/models.py
@@ -1,24 +1,11 @@
 """Common models for all api branches
 
 """
-
 import jsonschema
 from google.appengine.ext import ndb
 from google.appengine.datastore.datastore_query import Cursor
 
-
-# TODO: save in portfolio GUI project instead
-STUDENT_SCHEMA = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "title": "Student Schema",
-    "type": "object",
-    "properties": {
-        "lastName": {"type": "string"},
-        "firstName": {"type": "string"},
-        "matricule": {"type:": "string"},
-    },
-    "required": ["firstName", "lastName", "matricule"]
-}
+from api import schemas
 
 
 class Student(ndb.Model):
@@ -61,20 +48,24 @@ class Student(ndb.Model):
         return self.data.get('photo')
 
     @classmethod
-    @ndb.transactional(xg=True, retries=0)
     def new_student(cls, data):
         """Create a new student entity from its json representation
 
         """
-        existing_student = Student.get_by_id(data['matricule'])
-        if existing_student:
-            raise AttributeError(
-                "A student with a same matricule already exists."
-            )
+        cls.validate(data)
 
-        student = cls(id=data['matricule'], data=data)
-        student.put()
-        return student
+        @ndb.transactional(xg=True, retries=0)
+        def tx():
+            existing_student = Student.get_by_id(data['matricule'])
+            if existing_student:
+                raise AttributeError(
+                    "A student with a same matricule already exists."
+                )
+
+            student = cls(id=data['matricule'], data=data)
+            student.put()
+            return student
+        return tx()
 
     def _pre_put_hook(self):
         """Make sure data attribute is validate format some field
@@ -108,4 +99,4 @@ class Student(ndb.Model):
         """Validate student data schema.
 
         """
-        jsonschema.validate(data, STUDENT_SCHEMA)
+        schemas.student.validate(data)
